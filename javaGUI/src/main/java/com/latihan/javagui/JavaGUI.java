4be8a4f81de696829f50ec258975f4ac7095775e
Nurmansyah ado <adonurman@users.noreply.github.com>
2023-09-10 14:24:23 +0700

    nambah folder website

---
+++ javaGUI/src/main/java/com/latihan/javagui/JavaGUI.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.latihan.javagui;
+
+/**
+ *
+ * @author nurma
+ */
+public class JavaGUI {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
